3ee13ee7429bb3842c160aed7a014f92ddcfebf9
ebinaha <ebinha012@gmail.com>
2022-11-03 13:42:01 +0900

    business logic

--- src/main/java/miniproject/board/repository/JpaBoardRepository.java
+++ src/main/java/miniproject/board/repository/JpaBoardRepository.java
@@ -1,10 +1,16 @@
 package miniproject.board.repository;
 
+import miniproject.board.domain.Boards;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+/**
+ * 자동생성된 인터페이스이자 구현체
+ */
 @Repository
-public interface JpaBoardRepository extends JpaRepository {
-
-
+public interface JpaBoardRepository extends JpaRepository<Boards, Integer> {
+    //table Entity로 정의 / key??
+    //implement override 안하고 사용
 }
--- src/main/java/miniproject/board/service/BoardService.java
+++ src/main/java/miniproject/board/service/BoardService.java
@@ -1,12 +1,64 @@
 package miniproject.board.service;
 
+import miniproject.board.domain.Boards;
+import miniproject.board.repository.JpaBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * 게시글 관리 기능 구현
  */
 @Service
 public class BoardService {
+    @Autowired
+    private final JpaBoardRepository repository;
 
-    private String board;
+    public BoardService(JpaBoardRepository repository){
+        this.repository = repository;
+    }
+
+    /**
+     * 저장하는 비즈니스 로직
+     * @param boards
+     * @return
+     */
+    public Integer boardSave(Boards boards){
+        Boards board = repository.save(boards);
+        return board.getId();
+    }
+
+    /**
+     * 게시판리스트
+     * @return
+     */
+    public List<Boards> boardsList(){
+        List<Boards> boards = repository.findAll();
+        return boards;
+    }
+
+    /**
+     * id를 매개변수로 삭제하기
+     * @param id
+     */
+    public void boardDelete(Integer id){
+        repository.deleteById(id);
+    }
+
+    /**
+     * id를 매개변수로 조회하기
+     * @param id
+     * @return
+     */
+    public Boards board(Integer id){
+        //optional ==> 리스트 형태
+        Optional<Boards> boardslist = repository.findById(id);
+        return boardslist.get();
+    }
+
+
+
 }
